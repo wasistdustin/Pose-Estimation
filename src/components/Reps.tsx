dd3af016401d111f7d92612b41c25a8a480e11e4
dustin kohls <s0579625@htw-berlin.de>
2023-05-25 17:58:25 +0200

    del Y-Diff, add visibility + x coord (ankle/wirst)

--- src/components/Reps.tsx
+++ src/components/Reps.tsx
@@ -41,96 +41,152 @@ const Reps = ({ results }: Props) => {
       const landmarksArray = [results.poseLandmarks];
 
       // init important landmarks for angle or distance calculation - all right side
-      const landmarks1 = {
-        x: landmarksArray[0][16].x, //Wrist
+      const leftWrist = {
+        x: landmarksArray[0][15].x, //left Wrist
+        y: landmarksArray[0][15].y,
+      };
+      const leftShoulder = {
+        x: landmarksArray[0][11].x, //left Shoulder
+        y: landmarksArray[0][11].y,
+      };
+      const leftHip = {
+        x: landmarksArray[0][23].x, //left Hip
+        y: landmarksArray[0][23].y,
+      };
+      const leftElbow = {
+        x: landmarksArray[0][13].x, //left Ellbow
+        y: landmarksArray[0][13].y,
+      };
+      const leftAnkle = {
+        x: landmarksArray[0][27].x, //left Ankle
+        y: landmarksArray[0][27].y,
+      };
+      const rightWrist = {
+        x: landmarksArray[0][16].x, //right Wrist
         y: landmarksArray[0][16].y,
       };
-      const landmarks2 = {
-        x: landmarksArray[0][12].x, //Schulter
+      const rightShoulder = {
+        x: landmarksArray[0][12].x, //right Shoulder
         y: landmarksArray[0][12].y,
       };
-      const landmarks3 = {
-        x: landmarksArray[0][24].x, //Hip
+      const rightHip = {
+        x: landmarksArray[0][24].x, //right Hip
         y: landmarksArray[0][24].y,
       };
-      const landmarks4 = {
-        x: landmarksArray[0][14].x, //Ellbow
+      const rightElbow = {
+        x: landmarksArray[0][14].x, //right Elbow
         y: landmarksArray[0][14].y,
       };
-      const landmarks5 = {
-        x: landmarksArray[0][28].x, //Ankle
+      const rightAnkle = {
+        x: landmarksArray[0][28].x, //right Ankle
         y: landmarksArray[0][28].y,
       };
       // Middle one must be the origin
       const opt = { small: true, round: true };
+      //right Side
       //Calc Wrist-Ankle-Shoulder Angle
-      let anngle = findAngle(landmarks1, landmarks5, landmarks2, opt);
+      let angleAnkleR = findAngle(rightWrist, rightAnkle, rightShoulder, opt);
       //Calc Shoulder-Ellbow-Wrist Angle
-      let anngle1 = findAngle(landmarks2, landmarks4, landmarks1, opt);
+      let angleElbowR = findAngle(rightShoulder, rightElbow, rightWrist, opt);
       //Calc Shoulder-Hip-Ankle Angle
-      let anngle2 = findAngle(landmarks2, landmarks3, landmarks5, opt);
+      let angleHipR = findAngle(rightShoulder, rightHip, rightAnkle, opt);
+      //Left side
+      //Calc Wrist-Ankle-Shoulder Angle
+      let angleAnkleL = findAngle(leftWrist, leftAnkle, leftShoulder, opt);
+      //Calc Shoulder-Ellbow-Wrist Angle
+      let angleElbowL = findAngle(leftShoulder, leftElbow, leftWrist, opt);
+      //Calc Shoulder-Hip-Ankle Angle
+      let angleHipL = findAngle(leftShoulder, leftHip, leftAnkle, opt);
 
-      const angleSHW = `Handgelenk-Knöchel-Schulter: ${anngle}`;
-      const angleHAW = `Schulter-Elle-Handgelenk: ${anngle1}`;
-      const angleSHA = `Shoulder-Hip-Ankle ${anngle2}`; //works great -> almost 180
+      let visAnkleR = landmarksArray[0][28].visibility;
+      let visShoulderR = landmarksArray[0][12].visibility;
+      if (!visShoulderR) return;
+      if (!visAnkleR) return;
+
+      let visAnkleL = landmarksArray[0][27].visibility;
+      let visShoulderL = landmarksArray[0][11].visibility;
+      if (!visShoulderL) return;
+      if (!visAnkleL) return;
+
       const diffY =
         Math.abs(landmarksArray[0][28].y - landmarksArray[0][12].y) * 100;
 
-      setAngleText(angleSHW);
-      setAngleText1(angleHAW);
-      setAngleText2(angleSHA);
-      setDiffY(`Diff S&A: ${diffY}`);
-      setDistanceY(`Fixed Wert: ${distY}`);
+      setAngleText(`Handgelenk-Knöchel-Schulter: ${angleAnkleR}`);
+      setAngleText1(`Schulter-Elle-Handgelenk: ${angleElbowR}`);
+      setAngleText2(`Shoulder-Hip-Ankle ${angleHipR}`);
+      setDiffY(`Visibality ANkle ${rightAnkle.y}`);
+      setDistanceY(`Visabiliy ${rightWrist.y}`);
       //console.log(landmarksArray);
       setLandmarkArray(results);
-      //UP
+      //UP 3 angles & Ankle higher than Wrist (x-coord) & visibility from ankle & shoulder & 2 booleans
       if (
-        anngle > 30 &&
-        anngle1 > 170 &&
-        anngle2 > 170 &&
-        diffY > 0.9 * distY &&
-        !up &&
-        down
+        (angleAnkleR > 30 &&
+          angleElbowR > 170 &&
+          angleHipR > 170 &&
+          !up &&
+          down &&
+          visShoulderR > 0.8 &&
+          visAnkleR > 0.8 &&
+          rightAnkle.y < rightWrist.y) ||
+        (angleAnkleL > 30 &&
+          angleElbowL > 170 &&
+          angleHipL > 170 &&
+          !up &&
+          down &&
+          visShoulderL > 0.8 &&
+          visAnkleL > 0.8 &&
+          leftAnkle.y < leftWrist.y)
       ) {
         setUp(true);
-        setDirection(`Up ${down} ${up}`);
+        setDirection(`Last State: Up`);
         setDummyUp(`Up Y: ${diffY} `);
       }
       //DOWN
       else if (
-        anngle < 25 &&
-        anngle1 < 140 &&
-        diffY < 0.7 * distY &&
-        !down &&
-        !up
+        (angleAnkleR < 25 &&
+          angleElbowR < 140 &&
+          angleHipR > 150 &&
+          !down &&
+          !up &&
+          visShoulderR > 0.8 &&
+          visAnkleR > 0.8 &&
+          rightAnkle.y < rightWrist.y) ||
+        (angleAnkleL < 25 &&
+          angleElbowL < 140 &&
+          angleHipL > 150 &&
+          !down &&
+          !up &&
+          visShoulderL > 0.8 &&
+          visAnkleL > 0.8 &&
+          leftAnkle.y < leftWrist.y)
       ) {
         setDown(true);
-        setDirection(`Down ${down} ${up}`);
+        setDirection(`Last State: Down`);
       }
       //COUNTER
-      else if (up === true && down === true) {
+      else if (up && down) {
         setDown(false);
         setUp(false);
         setCounter(counter + 1);
       }
       //Save Init State (Y Distance between Ankle & Shoulder)
-      else if (
-        anngle > 30 &&
-        anngle1 > 170 &&
-        anngle2 > 170 &&
-        diffY > 0.9 * distY &&
-        initY
-      ) {
-        setDirection("Start");
-        setInitY(false);
-      }
-      // Calculate Init State
-      else if (initY) {
-        setDirection("Warten auf Positionierung");
-        setDistY(
-          Math.abs(landmarksArray[0][28].y - landmarksArray[0][12].y) * 100
-        );
-      }
+      // else if (
+      //   angleAnkleR > 30 &&
+      //   angleElbowR > 170 &&
+      //   angleHipR > 170 &&
+      //   diffY > 0.9 * distY &&
+      //   initY
+      // ) {
+      //   //setDirection("Start");
+      //   setInitY(false);
+      // }
+      // // Calculate Init State
+      // else if (initY) {
+      //   //setDirection("Warten auf Positionierung");
+      //   setDistY(
+      //     Math.abs(landmarksArray[0][28].y - landmarksArray[0][12].y) * 100
+      //   );
+      // }
     };
 
     updateReps();
@@ -139,8 +195,8 @@ const Reps = ({ results }: Props) => {
   return (
     <div style={{}}>
       {/* <Logger results={landmarkArray}></Logger> */}
-      Hier: {direction} <br /> {counter} <br /> {angleText} <br /> {angleText1}{" "}
-      <br /> {angleText2}
+      {direction} <br /> Wiederholungen: {counter} <br /> {angleText} <br />{" "}
+      {angleText1} <br /> {angleText2}
       <br /> {distanceY}
       <br /> {diffY}
       <br /> {dummy} {dummyUp}
